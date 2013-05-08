16ed5bad8e8ed11b18893863caab76a57c199815
gregpriday <gregpriday@965b8dba-cb3b-494d-9706-4adfdc746276>
2013-05-08 14:09:26 +0000

    Tagged Estate theme

---
+++ themes/estate/tags/1.0/js/top-slider.js
@@ -0,0 +1,18 @@
+jQuery(function($){
+
+    $(window).resize(function(){
+        // Initialize all the slides
+        $('#top-slider .slides .slide').each(function(){
+            var $$ = $(this);
+
+            newCss = {};
+            newCss.height = $$.find('.slide-contents').outerHeight();
+
+            if($$.data('background-image') != null) {
+                newCss['background-image'] = 'url(' + $$.data('background-image') + ')';
+            }
+
+            $$.css(newCss);
+        });
+    }).resize();
+});
