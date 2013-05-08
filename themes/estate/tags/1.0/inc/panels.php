ce49c08b960a3c1590d815e25a3d96c11148f840
gregpriday <gregpriday@965b8dba-cb3b-494d-9706-4adfdc746276>
2013-05-08 13:11:55 +0000

    Tagged Estate theme

---
+++ themes/estate/tags/1.0/inc/panels.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Integrates this theme with SiteOrigin panels page builder.
+ * 
+ * @package estate
+ * @since 1.0
+ * @license GPL 2.0
+ */
+
+/**
+ * Adds default page layouts
+ *
+ * @param $layouts
+ */
+function estate_prebuilt_page_layouts($layouts){
+	return $layouts;
+}
+add_filter('siteorigin_panels_prebuilt_layouts', 'estate_prebuilt_page_layouts');
+
+/**
+ * Configure the SiteOrigin page builder settings.
+ * 
+ * @param $settings
+ * @return mixed
+ */
+function estate_panels_settings($settings){
+	$settings['home-page'] = true;
+	return $settings;
+}
+add_filter('sitesiteorigin_panels_settings', 'estate_panels_settings');
