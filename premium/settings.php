6669a65a64cc4ffad4b9fbae29e8b08b0b88b14c
Greg Priday <greg@siteorigin.com>
2014-07-05 07:52:48 +0000

    * Ensured all photos are GPL compatible. * Removed hgroup tag. * Added step to remove template-blog.php page template. * Added new language files. * Improved support for new Page Builder home page. * Updated to latest version of FitVids * Premium: Added retina logo theme setting.

--- premium/settings.php
+++ premium/settings.php
@@ -5,6 +5,7 @@
  */
 function estate_premium_settings_init(){
 	siteorigin_settings_add_field('general', 'footer_attribution', 'checkbox');
+	siteorigin_settings_add_field('general', 'logo_retina', 'media');
 	siteorigin_settings_add_field('general', 'ajax_comments', 'checkbox');
 	siteorigin_settings_add_field('layout', 'responsive', 'checkbox');
 	siteorigin_settings_add_field('layout', 'responsive_menu', 'checkbox');
